ab86825f05b4d0bf8faedf5346f6281550a6921f
Navneet Kumar <navneetkumar9108@gmail.com>
2026-06-17 19:54:11 +0530

    How to create route basic in vue

---
+++ ch1/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+    <RouterLink to="/posts">Posts</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 24px;
+  border-bottom: 1px solid red;
+}
+
+nav a {
+  display: inline-block;
+  padding: 5px 10px;
+  text-decoration: none;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+nav a.router-link-exact-active {
+  background-color: red;
+  color: white;
+}
+</style>
