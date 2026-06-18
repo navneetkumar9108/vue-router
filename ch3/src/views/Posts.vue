0961502a601161d1ba5c408408866abf0fd15684
Navneet Kumar <navneetkumar9108@gmail.com>
2026-06-18 10:35:55 +0530

    404,redirect,alias

---
+++ ch3/src/views/Posts.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>Posts Page - All Posts</div>
+  <h1>Post 1</h1>
+  <h1>Post 2</h1>
+  <h1>Post 3</h1>
+  <h1>Post 4</h1>
+</template>
+
+<style scoped></style>
